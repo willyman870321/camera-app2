2e86eedf8ff157fab5b3c2d6b59973aebae0cb24
willyman870321 <50653106+willyman870321@users.noreply.github.com>
2020-12-01 20:38:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
-            track = stream.getTracks()[0];
+            track = stream.getTracks()[1];
             cameraView.srcObject = stream;
         })
         .catch(function(error) {
